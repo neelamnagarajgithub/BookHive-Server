772b45f336335a604a13c6d9d623df95b1a9c02c
Neelam Naga Raj <113004986+neelamnagarajgithub@users.noreply.github.com>
2024-03-25 00:03:01 +0530

    cors updating

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://book-hive-silk.vercel.app/",
+    origin: "https://book-hive-silk.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   })
